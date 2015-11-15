494099982399aeccad8c83ff21394fc060e858a8
xeviox-com <xeviox-com@users.noreply.github.com>
2015-11-15 16:25:56 +0100

    Move pages to separate folder.

--- app/startup.js
+++ app/startup.js
@@ -1,10 +1,13 @@
 define(['knockout', 'crossroads', 'hasher', 'jquery', './router'],
 		function(ko, crossroads, hasher, jquery, router) {
 
+	// components
 	ko.components.register('nav-bar', { require: 'components/nav-bar/nav-bar' });
-	ko.components.register('home-page', { require: 'components/home-page/home' });
+
+	// pages
+	ko.components.register('home-page', { require: 'pages/home-page/home' });
 	ko.components.register('about-page', {
-		template: { require: 'text!components/about-page/about.html' }
+		template: { require: 'text!components/pages/about-page/about.html' }
 	});
 
 	ko.applyBindings({ route: router.currentRoute });
